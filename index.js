8f3645e68ec57ba4c26258c3a9cff09de937643c
Motlatsi Lesole <lesolebt@gmail.com>
2026-01-12 09:27:10 +0200

    Update index.js with new edits

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ inquirer.prompt(questions).then((answers) => {
   if (username === VALID_USERNAME && password === VALID_PASSWORD) {
   console.log("\nLogin Successful\n");
   console.log("Project Name: Orange Fuels");
+  console.log("Description: Take full control of your VPS hosting. Access all the tools you need to run your online business smoothly.");
   console.log("Project ID: 670937XHFTYR6278DTR-ODHbtd6833-dIHFBIjuf");
   console.log("Domain: https://Orangefuels.co.za");
   console.log("Environment: Production");
@@ -36,7 +37,8 @@ inquirer.prompt(questions).then((answers) => {
   console.log("Cache Layer: Redis");
   console.log("Authentication: JWT Tokens");
   console.log("Hosting: Cloudflare + Nginx");
-  console.log("Dashboard: https://webuy.com/\n");
+  console.log("Dashboard: https://147.93.180.80/\n");
+  console.log("\x1b[31m\nDevelopment mode is unavailable on this device.\n\x1b[0m");
   } else {
     console.log("\x1b[31m\nInvalid credentials. Access denied.\n\x1b[0m");
     process.exit(1);
